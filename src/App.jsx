5c8b013378f3b442f52a5d37190e7a7456370077
unknown <taiwokolapo533@gmail.com>
2025-02-02 23:01:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {HashRouter as Router, Outlet,Routes, Route} from 'react-router-dom'
+
+import ColorGuessingGame from './ColorGuessingGame'
+
+
+const Layout =()=>{
+  return(
+  <>
+  
+  </>)
+}
+const App = () => {
+  return (
+    <>
+    <ColorGuessingGame />
+        
+    </>
+  )
+}
+
+export default App
